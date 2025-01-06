fb1e89b56b693a1a45244375ef8cc7ab19738a90
Calebbuffleben <calebbuffleben@gmail.com>
2025-01-06 18:45:41 -0300

    adding new things

--- client-server-challenge/server/server.go
+++ client-server-challenge/server/server.go
@@ -1,26 +1,60 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
 )
 
-func Server() {
+type CurrencyData struct {
+	USDBRL ExchangeRate `json:"USDBRL"`
+}
+
+type ExchangeRate struct {
+	Code       string `json:"code"`
+	Codein     string `json:"codein"`
+	Name       string `json:"name"`
+	High       string `json:"high"`
+	Low        string `json:"low"`
+	VarBid     string `json:"varBid"`
+	PctChange  string `json:"pctChange"`
+	Bid        string `json:"bid"`
+	Ask        string `json:"ask"`
+	Timestamp  string `json:"timestamp"`
+	CreateDate string `json:"create_date"`
+}
+
+func server(w http.ResponseWriter, r *http.Request) {
 	response, err := http.Get("https://economia.awesomeapi.com.br/json/last/USD-BRL")
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	responseData, err := io.ReadAll(response.Body)
+	defer response.Body.Close()
 
+	responseData, err := io.ReadAll(response.Body)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(string(responseData))
+
+	var currencyData CurrencyData
+	err = json.Unmarshal(responseData, &currencyData)
+	if err != nil {
+		http.Error(w, "Failed to parse JSON", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(currencyData)
 }
 
+func handleRequests() {
+	http.Handle("/getCoinValue", http.HandlerFunc(server))
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
 func main() {
-	Server()
+	handleRequests()
 }

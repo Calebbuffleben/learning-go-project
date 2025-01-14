4571ce6e388118fde344bc90ec03ba0461d9892e
Calebbuffleben <calebbuffleben@gmail.com>
2025-01-14 16:41:01 -0300

    db connection working

--- client-server-challenge/server/main.go
+++ client-server-challenge/server/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"encoding/json"
 	"fmt"
 	"io"
@@ -60,6 +61,10 @@ func server(w http.ResponseWriter, r *http.Request) {
 }
 
 func insertData(currencyData CurrencyData) error {
+	if db == nil {
+		return errors.New("database connection is not initialized")
+	}
+
 	insertDataQuery := `
 		INSERT INTO currency_data (
 			code,
@@ -97,8 +102,80 @@ func insertData(currencyData CurrencyData) error {
 }
 func handleRequests() {
 	http.Handle("/cotacao", http.HandlerFunc(server))
-	
+	http.Handle("/get-data", http.HandlerFunc(getData))
 	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func getData(w http.ResponseWriter, r *http.Request) {
+	dbPath := "./db/database.db"
+
+	// Ensure the db directory exists
+	if _, err := os.Stat("./db"); os.IsNotExist(err) {
+		err = os.Mkdir("./db", os.ModePerm)
+		if err != nil {
+			log.Fatalf("Failed to create db directory: %v", err)
+		}
+	}
+
+	// Open SQLite database
+	db, err := sql.Open("sqlite3", dbPath)
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %v", err)
+	}
+	defer db.Close()
+
+	if db == nil {
+		http.Error(w, "database connection is not initialized", http.StatusInternalServerError)
+		return
+	}
+
+	// Query to fetch data from the database
+	query := `
+		SELECT
+			code, codein, name, high, low, varBid, pctChange, bid, ask, timestamp, create_date 
+		FROM currency_data;`
+	
+	rows, err := db.Query(query)
+	if err != nil {
+		http.Error(w, "Failed to execute query", http.StatusInternalServerError)
+		return
+	}
+	defer rows.Close()
+
+	var results []ExchangeRate
+
+	for rows.Next() {
+		var result ExchangeRate
+		err = rows.Scan(
+			&result.Code,
+			&result.Codein,
+			&result.Name,
+			&result.High,
+			&result.Low,
+			&result.VarBid,
+			&result.PctChange,
+			&result.Bid,
+			&result.Ask,
+			&result.Timestamp,
+			&result.CreateDate,
+		)
+		if err != nil {
+			http.Error(w, "Failed to scan row", http.StatusInternalServerError)
+			return
+		}
+		results = append(results, result)
+	}
+
+	if err = rows.Err(); err != nil {
+		http.Error(w, "Failed to iterate over rows", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	if err := json.NewEncoder(w).Encode(results); err != nil {
+		http.Error(w, "Failed to encode JSON", http.StatusInternalServerError)
+		return
+	}
 }
 
 func main() {
